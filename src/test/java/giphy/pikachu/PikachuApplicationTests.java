8f0e4e7fb0861d795e56dac3d3efc83e7bde328d
yowai-hito <seow.h.y.95@gmail.com>
2022-04-05 15:36:09 +0800

    added jacoco

--- src/test/java/giphy/pikachu/PikachuApplicationTests.java
+++ src/test/java/giphy/pikachu/PikachuApplicationTests.java
@@ -3,11 +3,18 @@ package giphy.pikachu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import giphy.pikachu.Models.QueryObject;
+import giphy.pikachu.Services.SearchService;
+
 @SpringBootTest
 class PikachuApplicationTests {
 
 	@Test
 	void contextLoads() {
+		QueryObject test = new QueryObject();
+		SearchService testService = new SearchService();
+		Assert
+
 	}
 
 }
