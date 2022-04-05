a1ee1d2feeafa37c9278f1cb5bda9ffc83e02821
yowai-hito <seow.h.y.95@gmail.com>
2022-04-05 16:02:55 +0800

    remove test

--- src/test/java/giphy/pikachu/PikachuApplicationTests.java
+++ src/test/java/giphy/pikachu/PikachuApplicationTests.java
@@ -3,17 +3,11 @@ package giphy.pikachu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import giphy.pikachu.Models.QueryObject;
-import giphy.pikachu.Services.SearchService;
-
 @SpringBootTest
 class PikachuApplicationTests {
 
 	@Test
 	void contextLoads() {
-		QueryObject test = new QueryObject();
-		SearchService testService = new SearchService();
-
 	}
 
 }
