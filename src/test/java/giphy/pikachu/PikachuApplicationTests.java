f78bf300a239f8f146c33326ba9a878da94d7c2f
yowai-hito <seow.h.y.95@gmail.com>
2022-04-05 16:07:37 +0800

    ...

--- src/test/java/giphy/pikachu/PikachuApplicationTests.java
+++ src/test/java/giphy/pikachu/PikachuApplicationTests.java
@@ -3,11 +3,9 @@ package giphy.pikachu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import giphy.pikachu.Models.QueryObject;
+import giphy.pikachu.Services.SearchService;
+
 @SpringBootTest
 class PikachuApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
